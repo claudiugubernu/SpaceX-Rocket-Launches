a977aca230727dab3928042add583f10859759ab
Klaus Gubernu <claudiugubernu@gmail.com>
2022-02-11 09:35:09 +0000

    refactoring conde

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import {useEffect, useState} from "react";
 import Header from "./components/Header";
-import Launches from "./components/Launches";
+//import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
-import {Container} from "react-bootstrap";
+import FilterOptions from "./components/FilterOptions";
+import {Container, Row, Col } from "react-bootstrap";
+import HeroImage from "./components/HeroImage";
 
 function App() {
+  
 
-  const [launchesList, setLaunchList] = useState([]);
-
-  useEffect(()=> {
-    async function fetchLaunchList(){
-      const requestUrl = "https://api.spacexdata.com/v3/launches";
-      const response = await fetch(requestUrl);
-      const responseJSON = await response.json();
-      setLaunchList(responseJSON);
-    }
-    fetchLaunchList();
+  useEffect(() => { 
+    launchList();
   }, []);
+
+  const launchList = async () => {
+    const requestUrl = "https://api.spacexdata.com/v3/launches";
+    const response = await fetch(requestUrl);
+    const launchesData = await response.json();
+    
+    setLaunches(launchesData);
+  }
+
+  const [launches, setLaunches] = useState([]);
+  const [sortDescending, setSortDescending] = useState(launches);
 
   return (
     <>  
-        <Header />
-        <Container fluid className="app-body">
-          <Launches launchesList={launchesList} />
+      <Header />
+      <Container fluid className="app-body">
+        <FilterOptions 
+          setSortDescending={setSortDescending} 
+          launchList={launches}/>
+        <Container fluid className="launches-wrapper">  
+          <Row>
+            <Col xs={12} lg={5}>
+                <HeroImage />
+            </Col>
+            <Col xs={12} lg={7}>
+              <div className="launch-items">
+                {sortDescending.map((launch) => {
+                  return <Launch launch={launch} />
+                })}
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </Container>
     </>
   );
